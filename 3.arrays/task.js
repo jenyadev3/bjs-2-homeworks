5855126d3b9cdf40351d9a60fde6e88bdd735aca
jenyadev3 <112981213+jenyadev3@users.noreply.github.com>
2023-06-10 20:42:49 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let filteredPeople = users.filter(item => item.gender === gender);
+  let averageAge = filteredPeople.map(item => item.age).reduce((acc, curr) => acc + curr, 0) / filteredPeople.length;
+  return filteredPeople.length === 0 && ! filteredPeople.includes(gender) ? 0 : averageAge;
 }
